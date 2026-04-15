4761bc7b972ad746802e8b585f12ce3a06d3367b
Ashad Mansoor <ashadmansoor786@gmail.com>
2026-04-15 13:25:05 +0530

    Added file storage feature for student data persistence

--- src/StudentService.java
+++ src/StudentService.java
@@ -1,10 +1,18 @@
+import java.io.*;
 import java.util.ArrayList;
 
 public class StudentService {
     private final ArrayList<Student>students=new ArrayList<>();
+    private final String FILE_NAME="students.txt";
+
+    public StudentService() {
+        LoadFromFile();
+    }
+
     //Add Students
     public void addStudent(Student s){
         students.add(s);
+        saveToFile();
         System.out.println("Student added Successfully");
     }
     //View Students
@@ -23,6 +31,7 @@ public class StudentService {
         for(Student s:students){
             if(s.getId()==id){
                 students.remove(s);
+                saveToFile();
                 System.out.println("Student deleted Successfully");
                 found=true;
                 break;
@@ -38,12 +47,40 @@ public class StudentService {
             if(s.getId()==id){
                 s.setName(name);
                 s.setAge(age);
+                saveToFile();
                 System.out.println("Student updated Successfully");
                 return;
             }
         }
         if(students.isEmpty()){
             System.out.printf("No Students Found!");
+        }
+    }
+    private void saveToFile(){
+        try(BufferedWriter writer=new BufferedWriter(new FileWriter(FILE_NAME))) {
+            for(Student s:students){
+                writer.write(s.getId()+","+s.getName()+","+s.getAge());
+                writer.newLine();
+            }
+        }catch (IOException e){
+            System.out.println("Error Saving Data");
+        }
+    }
+
+    private void LoadFromFile(){
+        try(BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))){
+            String Line;
+
+            while((Line=reader.readLine())!=null){
+                String[] data=Line.split(",");
+                int id=Integer.parseInt(data[0]);
+                String name= data[1];
+                int age=Integer.parseInt(data[2]);
+
+                students.add(new Student(id,name,age));
+            }
+            }catch(IOException e){
+            System.out.println("No Privious data Found");
         }
     }
 }
